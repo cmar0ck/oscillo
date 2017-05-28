13c4e749206ba96c6222a17464f3af2f9c7a82c1
cmar0ck <cmar0ck@users.noreply.github.com>
2017-05-28 12:14:38 +0200

    Add files via upload

---
+++ oscilloscope-neighbors-new.php
@@ -0,0 +1,8 @@
+<pre><form method="post" enctype="multipart/form-data">GIF File: <input type="file" name="inputfile"><input type="submit"></form>No input file specified.
+
+Usage: php oscillo.php inputfile outputfile
+
+       Supported input formats are GIF and PNG images.
+       inputfile can be a directory containing PNG files.
+       Output will be a 48khz 8bit stereo WAV file. 
+       If your OS supports it (Cygwin/Linux), you can output to /dev/dsp for realtime playback.
